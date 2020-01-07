f3386d3f7729dc69653ba8e46d1712f7c5d4320e
TimKopp <timkopp@gmail.com>
2020-01-07 17:36:35 -0500

    added bootstrap test

---
+++ UnitTests/Bootstrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using SOM.Data;
+using SOM.Procedures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOM;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class Bootstrapper
+    {
+        [TestMethod]
+        public void BasePathCreated()
+        {
+            SOM.Bootstrapper.Run();
+            DirectoryInfo DI = new DirectoryInfo($"{AppSettings.BasePath}");
+            Assert.IsTrue(DI.Exists);
+        }
+       
+    }
+}
