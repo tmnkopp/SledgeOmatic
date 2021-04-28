9a8edf0f29a7a68931d3b54bf9257fc0c63a8c01
Tim Kopp <timkopp@gmail.com>
2021-04-28 19:48:02 -0400

    Add PatternIndexer

---
+++ CoreTests/Compiler/PatternIndexerTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SOM.IO;
+using SOM.Procedures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoreTests
+{
+    [TestClass]
+    public class PatternIndexerTest
+    {
+        [TestMethod]
+        public void PatternIndexer_Indexes()
+        { 
+            var s = Reader.Read(@"D:\dev\CyberScope\CyberScopeBranch\CSwebdev\database\DB_Update7.34_BOD_2021.sql");
+            ICompilable comp = new PatternIndexer("QGroup, (\\d{1,2}),",  1); 
+            var c = comp.Compile(s);
+            Cache.Inspect(c); 
+
+        }
+    }
+
+}
---
+++ SledgeOMatic/Procedures/Interpreters/PatternIndexer.cs
@@ -0,0 +1,37 @@
+﻿using SOM.Procedures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SOM.Extentions;
+using System.Text.RegularExpressions;
+
+namespace SOM.Procedures
+{
+    public class PatternIndexer : ICompilable
+    {
+        private int seed = 0;
+        private int _reset = 1;
+        private string Pattern = "[index]";
+        public PatternIndexer(string Pattern, int Seed)
+        {
+            this.seed = Seed;
+            this.Pattern = Pattern;
+        }
+        public string Compile(string content)
+        {
+            StringBuilder result = new StringBuilder();
+            string[] lines = content.Split('\n'); 
+            foreach (var line in lines) {
+                string rslt = Regex.Replace(line, this.Pattern, (Match m) => (
+                //"FOO"
+                m.Groups[0].Value.Replace(m.Groups[1].Value, (this.seed++).ToString())
+                ));
+                result.AppendFormat("{0}\n", rslt);
+            }
+            return result.ToString().TrimTrailingNewline();
+        }
+       
+    }
+}
