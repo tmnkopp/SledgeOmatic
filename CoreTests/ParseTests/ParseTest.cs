6e92813f463727e6b07a99124566b37e511c8a4d
Tim Kopp <timkopp@gmail.com>
2021-10-19 09:17:34 -0400

    update eins refactor unit test

--- CoreTests/ParseTests/ParseTest.cs
+++ CoreTests/ParseTests/ParseTest.cs
@@ -19,8 +19,8 @@ namespace UnitTests
         public void LineExtractor_Extracts()
         {
             DirectoryParser parser = new DirectoryParser();
-            parser.Directory = "D:\\dev\\CyberScope\\CyberScopeBranch\\CSwebdev\\database\\*.*";
-            parser.Parser = new LineExtractor("Agency Level Questions", 5);
+            parser.Directory = "D:\\dev\\CyberScope\\CyberScopeBranch\\CSwebdev\\database\\Archive\\*.*";
+            parser.Parser = new LineExtractor("HVAIn", 1);
             parser.Parser.ParseMode = ParseMode.Verbose;
             parser.Inspect();
             Assert.IsNotNull(parser);
