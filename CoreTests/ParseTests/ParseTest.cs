763efce72693f6d492828030f3376cca7bc990ee
Tim Kopp <timkopp@gmail.com>
2021-10-28 13:24:49 -0400

    update eins refactor unit test

--- CoreTests/ParseTests/ParseTest.cs
+++ CoreTests/ParseTests/ParseTest.cs
@@ -21,7 +21,7 @@ namespace UnitTests
             Cache.Write("");
             StringBuilder sb = new StringBuilder();
             DirectoryParser parser = new DirectoryParser(); 
-            parser.Parser = new LineExtractor("KEY_SES_CAUSER", 1);
+            parser.Parser = new LineExtractor("ddl_Sections", 1);
             parser.Parser.ParseMode = ParseMode.Verbose;
             parser.PathExcludePattern = @"Archive|bin\\|obj\\"; 
             //parser.ParseDirectory("D:\\dev\\CyberScope\\CyberScopeBranch\\CSwebdev\\database\\*.sql");
