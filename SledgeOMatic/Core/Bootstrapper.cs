14a83b91407a9a0b672b3e7d3dc47a311967facb
Tim Kopp <timkopp@gmail.com>
2022-01-20 19:26:13 -0500

    compilers refactor 66103

--- SledgeOMatic/Core/Bootstrapper.cs
+++ SledgeOMatic/Core/Bootstrapper.cs
@@ -16,8 +16,7 @@ namespace SOM
             if (string.IsNullOrEmpty(envar))
             {
                 Environment.SetEnvironmentVariable("som", "c:\\_som\\", EnvironmentVariableTarget.User); 
-            }
-
+            } 
             DirectoryInfo DI;
             string[] dirnames = new string[] {
                 AppSettings.BasePath,  AppSettings.SourceDir, AppSettings.DestDir 
@@ -25,9 +24,8 @@ namespace SOM
             foreach (string dir in dirnames)
             {
                 DI = new DirectoryInfo(dir);
-                if (!DI.Exists)  Directory.CreateDirectory(dir);
-            }
-    
+                if (!DI.Exists) Directory.CreateDirectory(dir);
+            } 
             string[] filenames = new string[] { "_cache.txt"  }; 
             foreach (string filename in filenames)
             {
