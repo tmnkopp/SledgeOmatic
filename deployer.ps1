1e374c25b1a6244b556cf78e006d5d9912fe0396
Tim Kopp <timkopp@gmail.com>
2022-04-07 16:42:06 -0400

    compilers refactor 11669

--- deployer.ps1
+++ deployer.ps1
@@ -6,15 +6,14 @@
     ) 
     if ($with -match ' release ' ){ 
         cd 'C:\Users\Tim\source\repos\SledgeOMatic'; 
-        dotnet build --configuration Debug ;
-        dotnet build --configuration Release ;
+        dotnet build --configuration Debug ; dotnet build --configuration Release ;
         dotnet publish SledgeOMatic -p:PublishProfile=FolderProfile ; 
         Copy-Item -Path C:\Users\Tim\source\repos\SledgeOMatic\SledgeOMatic\bin\publish\SOM.exe -Destination c:\_som\SOM.exe -Force  
     }
     if ($with -match ' commit ' ){
         $m = -join ((65..90) + (97..122) | Get-Random -Count 2 | % {[char]$_ +''+ $_ }) 
         cd 'C:\Users\Tim\source\repos\SledgeOMatic';  
-        git add .; git commit -m ('refactor context' + $m) ; git push; 
+        git add .; git commit -m ('refactor context ' + $m) ; git push; 
     }   
     explorer.exe C:\_som\
 } 
