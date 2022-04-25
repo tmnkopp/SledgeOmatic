0d2ab62cc2aea686ab10a831de9d7c47dee4cd0a
Tim Kopp <timkopp@gmail.com>
2022-04-25 13:04:37 -0400

    compilers refactor 10379

--- SledgeOMatic/Procedures/Interpreters/Insert.cs
+++ SledgeOMatic/Procedures/Interpreters/Insert.cs
@@ -7,30 +7,30 @@ namespace SOM.Procedures
 {
     public class Insert : ICompilable
     {
-        private string SearchPattern;
-        private string NewContent;
+        private string _searchPattern;
+        private string _newContent;
         private string _format;
         public Insert(string SearchPattern, string Format, string NewContent)
         {
-            this.SearchPattern = SearchPattern;
-            this.NewContent = NewContent;
+            this._searchPattern = SearchPattern;
+            this._newContent = NewContent.Replace(@"\n", System.Environment.NewLine);
             this._format = Format.Replace(@"\n", System.Environment.NewLine);
         }
         public string Compile(ISomContext somContext)
         {
             string content = somContext.Content;
-            if (Regex.IsMatch(content, this.SearchPattern))
+            if (Regex.IsMatch(content, this._searchPattern))
             {
-                content = Regex.Replace(content, this.SearchPattern,
+                content = Regex.Replace(content, this._searchPattern,
                     m =>
                     {
                         if (m.Groups.Count > 0)
                         {
                             return this._format
-                            .Replace("$1", this.NewContent)
+                            .Replace("$1", this._newContent)
                             .Replace("$2", m.Groups[0].Value);
                         }
-                        return this.NewContent;
+                        return this._newContent;
                     }
                     , RegexOptions.Singleline);
             };
