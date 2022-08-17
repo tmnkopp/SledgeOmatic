bc370bd7e06fb099cda44eb3c3bcc4a7421132ad
Tim Kopp <timkopp@gmail.com>
2022-08-17 09:22:08 -0400

    compilers refactor 8780

--- CoreTests/ParseTests/ParamExtract.cs
+++ CoreTests/ParseTests/ParamExtract.cs
@@ -50,66 +50,14 @@ namespace CoreTests
         public void Field_extractor()
         {
             ICompilable obj;
-            obj = new CompilableFactory().Create(typeof(Insert), " -p /p Prop /p Prop1 ");
+            obj = new GenericFactory<ICompilable>().Create("SOM.Procedures.Insert", @"  
+                -p /p Prop /p Prop1 /p Prop2 
+            ");
             Console.Write(new { obj });
-            obj = new CompilableFactory().Create(typeof(Insert), "  -p /p:Prop=value /p:Prop1=value1   ");
+            obj = new GenericFactory<ICompilable>().Create("SOM.Procedures.Insert", @"   
+                -p /p:Prop=value /p:Prop1=value1    
+            ");
             Console.Write(new { obj });
         } 
-    }
-    public class CompilableFactory{
- 
-        private ICompilable obj;
-        public CompilableFactory()
-        {
-        }
-        public ICompilable Create(Type type, string paramString)
-        {
-            paramString = paramString.Split("\n")[0];
-            var m = Regex.Match(paramString, $"(/p:.*)\n");
-            if (m.Success)
-            { 
-                var props = new Dictionary<string, string>();
-                foreach (var item in Regex.Split(m.Groups[1].Value, $"/p:"))
-                {
-                    if (item.Contains("="))
-                        props.Add(item.Split("=")[0], item.Split("=")[1]); 
-                }
-
-                this.obj = (ICompilable)Activator.CreateInstance(type);
-
-                (from p in obj.GetType().GetProperties()
-                 where props.ContainsKey(p.Name)
-                 select p).ToList().ForEach(p => {
-                     p.SetValue(obj, props[p.Name], null);
-                 });
-            }
-
-            m = Regex.Match(paramString, $@"(/p\s.*)\n");
-            if (m.Success)
-            {
-                var oparms = new List<object>();
-                foreach (var item in Regex.Split(m.Groups[1].Value, $"/p\\s"))
-                {
-                    if (!string.IsNullOrWhiteSpace(item))
-                        oparms.Add(item);
-                }
-                this.obj = (ICompilable)Activator.CreateInstance(type, oparms.ToArray());
-            }
-            return obj;
-        }
-        public ICompilable Create(Type type, ParameterInfo[] oparms)
-        {
-            this.obj = (ICompilable)Activator.CreateInstance(type, oparms.ToArray());
-            return obj;
-        }
-        public ICompilable Create(Type type, Dictionary<string, string> props)
-        { 
-            this.obj = (ICompilable)Activator.CreateInstance(type);
-            (from p in obj.GetType().GetProperties() 
-             where props.ContainsKey(p.Name) select p).ToList().ForEach(p=>{
-                p.SetValue(obj, props[p.Name], null);
-            }); 
-            return obj;
-        } 
-    }
+    } 
 }
---
+++ SledgeOMatic/Core/GenericFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace SOM.Core
+{
+    public class GenericFactory<T>
+    {
+        private T obj;
+        public GenericFactory()
+        {
+        }
+        public T Create(string FullTypeName, string paramString)
+        {
+            Type type = Type.GetType($"{FullTypeName}, SOM");
+            paramString = Regex.Replace(paramString, $@"\r|\t", "");
+            var m = Regex.Match(paramString, $@"(/p:.*)\n");
+            if (m.Success)
+            {
+                var props = new Dictionary<string, string>();
+                foreach (var item in Regex.Split(m.Groups[1].Value, $"/p:"))
+                {
+                    if (item.Contains("="))
+                        props.Add(item.Split("=")[0], item.Split("=")[1].TrimEnd());
+                }
+
+                this.obj = (T)Activator.CreateInstance(type);
+
+                (from p in obj.GetType().GetProperties()
+                 where props.ContainsKey(p.Name)
+                 select p).ToList().ForEach(p => {
+                     p.SetValue(obj, props[p.Name], null);
+                 });
+            }
+
+            m = Regex.Match(paramString, $@"(/p\s.*)\n");
+            if (m.Success)
+            {
+                var oparms = new List<object>();
+                foreach (var item in Regex.Split(m.Groups[1].Value, $"\\s/p\\s"))
+                {
+                    if (!string.IsNullOrWhiteSpace(item))
+                        oparms.Add(item.TrimEnd());
+                }
+                this.obj = (T)Activator.CreateInstance(type, oparms.ToArray());
+            }
+            return this.obj;
+        }
+    }
+}
