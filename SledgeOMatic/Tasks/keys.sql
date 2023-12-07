aa6b71c77b72a38e182b87fb718a9cc7aadaacc0
Tim Kopp <timkopp@gmail.com>
2023-12-07 13:18:30 -0500

    compilers refactor 101114

--- SledgeOMatic/Tasks/keys.sql
+++ SledgeOMatic/Tasks/keys.sql
@@ -2,5 +2,6 @@ SELECT MAX(PK_ReportingCycle) PK_ReportingCycle FROM fsma_ReportingCycles
 SELECT MAX(PK_QuestionGroup) PK_QuestionGroup FROM fsma_QuestionGroups
 SELECT MAX(PK_FormPage) PK_FormPage FROM fsma_FormPages
 SELECT MAX(PK_Question) PK_Question FROM fsma_Questions
-
-SELECT * FROM vwQuestions WHERE PK_FORM = '2023-Q4-CIO' 
+ 
+SELECT MAX(PK_Question) FROM vwQuestions WHERE PK_FORM = '2024-Q1-CIO'   
+ 
