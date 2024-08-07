fce5fe11996f0fe1390ebc002f49712a77a9420b
Tim Kopp <timkopp@gmail.com>
2024-08-07 09:57:49 -0400

    keys updated

--- SledgeOMatic/Tasks/keys.sql
+++ SledgeOMatic/Tasks/keys.sql
@@ -3,13 +3,15 @@ SELECT (SELECT MAX(PK_ReportingCycle)  FROM fsma_ReportingCycles) PK_ReportingCy
 , (SELECT MAX(PK_FormPage)  FROM fsma_FormPages) PK_FormPage
 , (SELECT MAX(PK_Question)  FROM fsma_Questions) PK_Question 
 
-SELECT MIN(PK_Question), MAX(PK_Question) FROM vwQuestions WHERE PK_FORM = '2024-Q3-CIO'  
-SELECT MIN(FK_FormPage), MAX(FK_FormPage) FROM vwQuestions WHERE PK_FORM = '2024-Q3-CIO' 
-SELECT MIN(PK_QuestionGroup), MAX(PK_QuestionGroup) FROM vwQuestions WHERE PK_FORM = '2024-Q3-CIO'   
+SELECT MIN(PK_Question), MAX(PK_Question) FROM vwQuestions WHERE PK_FORM = '2023-A-SAOP'  
+SELECT MIN(FK_FormPage), MAX(FK_FormPage) FROM vwQuestions WHERE PK_FORM = '2023-A-SAOP' 
+SELECT MIN(PK_QuestionGroup), MAX(PK_QuestionGroup) FROM vwQuestions WHERE PK_FORM = '2023-A-SAOP'   
  
 SELECT * FROM fsma_ReportingCycles WHERE Description LIKE '%CIO%' ORDER BY PK_ReportingCycle DESC
 
-SELECT * FROM vwOrgSubtoComponent WHERE PK_FORM='2024_A_IG' AND Component='Department of Justice'
-SELECT * FROM vwQuestions WHERE PK_FORM='2024-Q4-MFA' 
+SELECT * FROM vwOrgSubtoComponent WHERE PK_FORM='2023-A-SAOP' AND Component='Department of Justice'
+SELECT * FROM vwQuestions WHERE PK_FORM='2023-A-SAOP' 
 
+
+ 
 
